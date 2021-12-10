6490759d01899b557cccdeafc41de83cff7536a4
mike12699 <mariomania85@gmail.com>
2021-12-10 13:08:49 -0600

    Adds the project

--- main.cpp
+++ main.cpp
@@ -1,1 +1,123 @@
-int main() { return 0; }
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Player {
+    int tot;
+    bool bj;
+public:
+    void play()
+    {
+        srand(time(0));
+        int a = rand() % 10 + 1;
+        int b = rand() % 10 + 1;
+        cout << "first cards" << a << "," << b << endl;
+        tot = a + b;
+        cout << "Total:" << tot << endl;
+        if (tot == 21)
+        {
+            bj = true;
+        }
+        else
+            bj = false;
+        char op;
+        while (tot <= 21)
+        {
+            if (tot >= 17) {
+                cout << "do you want another card?(y/n):";
+                cin >> op;
+                if (op != 'y') {
+                    break;
+                }
+            }
+            int c = rand() % 10 + 1;
+            cout << "Card:" << c << endl;
+            tot += c;
+            cout << "Total:" << tot << endl;
+        }
+    }
+    int getTotal()
+    {
+        return tot;
+    }
+    bool isBlackJack()
+    {
+        return bj;
+    }
+};
+class Dealer {
+    int tot;
+    bool bj;
+
+public:
+    void play()
+    {
+        srand(time(0));
+        int a = rand() % 10 + 1;
+        int b = rand() % 10 + 1;
+        cout << "first cards" << a << "," << b << endl;
+        tot = a + b;
+        cout << "Total:" << tot << endl;
+        if (tot == 21) {
+            bj = true;
+        }
+        else
+            bj = false;
+        while (tot < 17) {
+            int c = rand() % 10 + 1;
+            cout << "Card:" << c << endl;
+            tot += c;
+            cout << "Total:" << tot << endl;
+        }
+    }
+    int getTotal()
+    {
+        return tot;
+    }
+    bool isBlackJack()
+    {
+        return bj;
+    }
+};
+int main()
+{
+    Player p;
+    Dealer d;
+    char op;
+    do {
+        p.play();
+        cout << "Player Cards in hand:" << p.getTotal() << endl;
+        d.play();
+        cout << "Dealer Cards in hand:" << d.getTotal() << endl;
+        if (p.getTotal() > 21) {
+            cout << "Game Result:Player Bursts" << endl;
+        }
+        else if (p.isBlackJack()) {
+            cout << "Game Result:Player BlackJack" << endl;
+        }
+        else if (d.isBlackJack()) {
+            cout << "Game Result:Dealer BlackJack" << endl;
+        }
+        else if (p.getTotal() > 21 && d.getTotal() <= 21) {
+            cout << "Game Result:Player Burst" << endl;
+        }
+        else if (d.getTotal() > 21 && p.getTotal() <= 21) {
+            cout << "Game Result:Player Wins" << endl;
+        }
+        else if (d.getTotal() == p.getTotal()) {
+            cout << "Game Result:Push" << endl;
+        }
+        else if (d.getTotal() < p.getTotal()) {
+            cout << "Game Result:Player Wins" << endl;
+        }
+        else if (d.getTotal() > p.getTotal()) {
+            cout << "Game Result:Player Bursts" << endl;
+        }
+        cout << "Do you want to continue?(y/n):";
+        cin >> op;
+        if (op != 'y') {
+            break;
+        }
+    } while (true);
+}
--- test.cpp
+++ test.cpp
@@ -1,2 +1,31 @@
 #include "utils.h"
-int main() {}
+#include "main.cpp"
+
+class PlayerTestsHarness : public AbstractTestHarness{
+    private:
+        Player p = Player();
+    public:
+        PlayerTestsHarness() {
+            register_test_func("Player Test", [this]() -> void { assert_equal(21, p.getTotal());});
+        }
+};
+
+class DealerTestHarness : public AbstractTestHarness{
+    private:
+        Dealer d = Dealer();
+    public:
+        DealerTestHarness() {
+            register_test_func("Dealer Test", [this]() -> void {assert_equal(21, d.getTotal());});
+        }
+};
+
+class GlobalTestManager : public TestManager{
+    public:
+        GlobalTestManager() { add_test("Player Tests", PlayerTestsHarness());}
+        GlobalTestManager() { add_test("Dealer Tests", DealerTestHarness());}
+};
+
+int main() {
+    GlobalTestManager tr = GlobalTestManager();
+    tr.execute();
+}
